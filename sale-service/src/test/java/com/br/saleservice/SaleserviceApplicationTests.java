ad28760bce7a4f6786443f25d8abeba678e023ce
reginaldo.silva <reginaldo.silva@sap.ce.gov.br>
2026-03-07 11:43:28 -0300

    adicionando tudo

---
+++ sale-service/src/test/java/com/br/saleservice/SaleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
