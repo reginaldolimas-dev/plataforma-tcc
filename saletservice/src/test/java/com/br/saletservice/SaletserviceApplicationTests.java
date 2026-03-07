9654770181b1613612e1e980cd3962218083cd92
reginaldo.silva <reginaldo.silva@sap.ce.gov.br>
2026-03-07 11:36:26 -0300

    first commit

---
+++ saletservice/src/test/java/com/br/saletservice/SaletserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.saletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaletserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
