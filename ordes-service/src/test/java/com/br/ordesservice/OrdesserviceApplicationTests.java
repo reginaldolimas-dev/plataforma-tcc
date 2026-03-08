82030482a414994aed649a09e2cd26a6a399da28
reginaldo.silva <reginaldo.silva@sap.ce.gov.br>
2026-03-08 19:26:56 -0300

    ajustando

---
+++ ordes-service/src/test/java/com/br/ordesservice/OrdesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ordesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdesserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
