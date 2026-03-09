b2e990ce76ddad2f57f4d244c70123a620c4c64b
Reginaldo Silva <reginaldo.silva@sap.ce.gov.br>
2026-03-09 13:33:14 -0300

    adiciona anotação @Data à classe ProductEntity e renomeia tabela para "product"

--- product-service/src/main/java/com/br/productservice/model/entity/ProductEntity.java
+++ product-service/src/main/java/com/br/productservice/model/entity/ProductEntity.java
@@ -6,11 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "products")
+@Data
+@Table(name = "product")
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
