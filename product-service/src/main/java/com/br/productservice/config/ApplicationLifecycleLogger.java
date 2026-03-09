a24026b497db46b98feb6c5e5188e004259ed2c4
Reginaldo Silva <reginaldo.silva@sap.ce.gov.br>
2026-03-09 13:04:51 -0300

    atualiza pacotes e adiciona logs de ciclo de vida no serviço de produtos

--- product-service/src/main/java/com/br/productservice/config/ApplicationLifecycleLogger.java
+++ product-service/src/main/java/com/br/productservice/config/ApplicationLifecycleLogger.java
@@ -1,4 +1,4 @@
-package com.br.customerservice.config;
+package com.br.productservice.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- product-service/src/main/java/com/br/productservice/service/ProductService.java
+++ product-service/src/main/java/com/br/productservice/service/ProductService.java
@@ -5,6 +5,8 @@ import com.br.productservice.dto.ProductCreateDTO;
 import com.br.productservice.dto.ProductResumeDTO;
 import com.br.productservice.dto.ProductUpdateDTO;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,15 +16,21 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository repository;
 
+    private static final Logger log = LoggerFactory.getLogger(ProductService.class);
+
     public Page<ProductResumeDTO> findAllPaginated(Pageable pageable) {
         return repository.findAllPaginated(pageable);
     }
 
     public void saveProduct(ProductCreateDTO product) {
+        log.info("Creating Product");
         repository.saveProduct(product);
+        log.info("Product created");
     }
 
     public void updateProduct(ProductUpdateDTO product) {
+        log.info("Updating Product with id: {}", product.getId());
         repository.updateProduct(product);
+        log.info("Product updated with id: {}", product.getId());
     }
 }
