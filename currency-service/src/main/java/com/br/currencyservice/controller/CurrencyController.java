ec66a7c90664fdea28794cb466183bd59618df11
reginaldo.silva <reginaldo.silva@sap.ce.gov.br>
2026-05-02 12:08:14 -0300

    adiciona logs para requisições em CurrencyController

--- currency-service/src/main/java/com/br/currencyservice/controller/CurrencyController.java
+++ currency-service/src/main/java/com/br/currencyservice/controller/CurrencyController.java
@@ -4,6 +4,8 @@ import com.br.currencyservice.dto.ApiResponse;
 import com.br.currencyservice.dto.CurrencyResponseDTO;
 import com.br.currencyservice.service.CurrencyService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +20,11 @@ import java.util.List;
 @RequestMapping("/currency")
 public class CurrencyController {
     private final CurrencyService service;
+    private static final Logger log = LoggerFactory.getLogger(CurrencyController.class);
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<CurrencyResponseDTO>>> getAll() {
+        log.info("Receiving request to retrieve all currencies");
 
         long startTime = System.currentTimeMillis();
         List<CurrencyResponseDTO> result = service.getAll();
@@ -38,6 +42,7 @@ public class CurrencyController {
 
     @GetMapping("/{code}")
     public ResponseEntity<ApiResponse<CurrencyResponseDTO>> findByCode(@PathVariable String code) {
+        log.info("Receiving request to find currency by code: {}", code);
 
         long startTime = System.currentTimeMillis();
         var result = service.findByCode(code);
