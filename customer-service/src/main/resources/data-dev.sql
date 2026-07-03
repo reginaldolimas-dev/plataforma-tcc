7b099f1df04ed52198f31c39d6d5777712c72ce5
reginaldo.silva <reginaldo.silva@sap.ce.gov.br>
2026-07-03 16:23:27 -0300

    adiciona configuração de profiles dev e prod, suporte ao H2 para desenvolvimento, e script de dados de exemplo

---
+++ customer-service/src/main/resources/data-dev.sql
@@ -0,0 +1,1 @@
+insert into usuarios (email, name, role, password, usuario_login) values( 'aluno@aluno.com', 'Aluno', 'user', '$2a$10$5sevjfX7kaLdn3HopGDKHeYFbfvE0GhKvjUz83YsmRZi3D7SRSvpi', 'aluno')
